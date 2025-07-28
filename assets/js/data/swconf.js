8627e9172d078ae34d69d1de56a1c64cb38b075b
yikemango <yikemango@users.noreply.github.com>
2025-07-28 08:06:25 +0000

    deploy: cae42483dee9206391f2f17a37f9097eb1b24fb0

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1753689985',resources: [
+      '/assets/css/.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/about/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
